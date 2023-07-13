015a006dac18bef0b53e5af35e1658f10387bddd
notfirefox <138280734+notfirefox@users.noreply.github.com>
2023-07-13 17:31:46 +0200

    Cleanup

--- init.zsh
+++ init.zsh
@@ -14,17 +14,11 @@ SAVEHIST=1000
 # enable colored output for ls
 alias ls='ls --color=auto --group-directories-first'
 
-# fix for zsh-syntax-highlighting
-# edit: does not seem to be necessary anymore
-#ZVM_INIT_MODE='sourcing'
-
 # allow case insensitive completion
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 
-# create alias for updating zsh plugins
-alias zsh-update="git -C ${0:a:h} submodule update --remote --merge"
-
 # source zsh plugins
-source "/usr/share/zsh/plugins/zsh-vi-mode/zsh-vi-mode.zsh"
-source "/usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
-source "/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+PLUGINS="/usr/share/zsh/plugins"
+source "$PLUGINS/zsh-vi-mode/zsh-vi-mode.zsh"
+source "$PLUGINS/zsh-autosuggestions/zsh-autosuggestions.zsh"
+source "$PLUGINS/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
