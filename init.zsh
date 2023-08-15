f14203e5a260360c85b7acff060f8595f43ca92c
notfirefox <138280734+notfirefox@users.noreply.github.com>
2023-08-15 14:31:54 +0200

    Fix fedora build

--- init.zsh
+++ init.zsh
@@ -17,11 +17,12 @@ alias ls='ls --color=auto --group-directories-first'
 # allow case insensitive completion
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 
-# bind autosuggest acception to control space
-bindkey '^ ' autosuggest-accept
-
 # source zsh plugins
-PLUGINS="/usr/share"
-#source "$PLUGINS/zsh-vi-mode/zsh-vi-mode.zsh"
-source "$PLUGINS/zsh-autosuggestions/zsh-autosuggestions.zsh"
-source "$PLUGINS/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+source "$HOME/.local/share/zsh-vi-mode/zsh-vi-mode.zsh"
+source "/usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+source "/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+
+# accept auto suggestion using ctrl space
+function zvm_after_init() {
+  zvm_bindkey viins '^ ' autosuggest-accept
+}
