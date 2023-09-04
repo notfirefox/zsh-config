83e1176267ca67c4d5ab7d17d0373db5f66ce252
notfirefox <138280734+notfirefox@users.noreply.github.com>
2023-09-04 12:22:20 +0200

    Remove restic alias

--- init.zsh
+++ init.zsh
@@ -17,12 +17,6 @@ then
 fi
 export PATH
 
-# inject environment variables into restic
-alias restic='env \
-  RESTIC_REPOSITORY=$(secret-tool lookup password RESTIC_REPOSITORY)\
-  RESTIC_PASSWORD=$(secret-tool lookup password RESTIC_PASSWORD)\
-  restic'
-
 # enable colored output for grep and ls
 alias grep='grep --color=auto'
 alias ls='ls --color=auto --group-directories-first'
